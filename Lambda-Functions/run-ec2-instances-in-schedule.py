e25ef86f03f35ce5c52cd89db8d522658c29475f
Dinesh <calltomdinesh@gmail.com>
2018-12-08 10:47:26 +0530

    ec2 schedule

---
+++ Lambda-Functions/run-ec2-instances-in-schedule.py
@@ -0,0 +1,23 @@
+# This Lambda function runs EC2 instnaces in schedule
+
+import json
+import boto3
+
+ec2 = boto3.resource('ec2')
+
+def lambda_handler(event, context):
+    filter = [
+        {
+            'Name': 'tag:Type', 
+            'Values': ['Scheduled']
+            
+        }
+    ]   
+    instances = ec2.instances.filter(Filters=filter)
+
+    for instance in instances:
+        print(instance)
+        instance.start()
+
+    return str(instance)
+
